197c23546a80b74b04e682fdd8ccb34ac4d4b732
Shimasawa <honnomushi@rakuten.jp>
2022-04-21 14:47:16 +0900

    help・作りかけ

--- cogs/help.py
+++ cogs/help.py
@@ -4,10 +4,21 @@ from discord.ext import commands
 class Help(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-    
+        self.cogdict = dict(self.bot.cogs)
+
     @commands.command()
-    async def help(self,ctx):
-        await ctx.reply("hey")
+    async def help(self,ctx,command=None):
+        if command == None:
+            def check(msg):
+                return int(msg) in range(len(self.cogdict))  and msg.author == ctx.author
+            cogpool = []
+            for num,name in enumerate(self.cogdict.keys()):
+                cogpool.append(f"{num} : {name}")
+            helpmsg = "\n".join(cogpool)
+            await ctx.send(embed=discord.Embed(
+                title="HELP",
+                description=helpmsg
+            ))
 
 def setup(bot):
     return bot.add_cog(Help(bot))
