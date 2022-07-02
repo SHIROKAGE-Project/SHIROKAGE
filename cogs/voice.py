48aa9894671e0f1376efbeb7081d5b7ca358e288
Shimasawa <honnomushi@rakuten.jp>
2022-07-02 21:15:20 +0900

    鯖毎の切断対応と例外処理

--- cogs/voice.py
+++ cogs/voice.py
@@ -7,12 +7,20 @@ class Voice(commands.Cog):
         self.voice = None
 
     @commands.command()
-    async def connect(self,ctx):
-        self.voice = await discord.VoiceChannel.connect(ctx.author.voice.channel)
+    async def join(self,ctx):
+        if ctx.author.voice is None:
+            await ctx.send("先にボイスチャンネルに接続してください")
+            return
+        self.voice = await ctx.author.voice.channel.connect()
+        await ctx.send("接続しました")
     
     @commands.command()
-    async def disconnect(self,ctx):
-        await self.voice.disconnect()
+    async def leave(self,ctx):
+        if ctx.guild.voice_client is None:
+            await ctx.send("ボイスチャンネルに接続していません")
+            return
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("切断しました")
     
 def setup(bot):
     return bot.add_cog(Voice(bot))
